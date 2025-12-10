21d227f3267d20707fe12622b713982ebe009d4c
Cyndja Aryelle <168868280+aryzinhasz@users.noreply.github.com>
2025-12-10 19:37:15 -0300

    Create errorHandler.js

---
+++ errorHandler.js
@@ -0,0 +1,5 @@
+function handleDatabaseError(error) {
+  console.error("Erro no banco de dados:", error);
+  // Trate o erro conforme as necessidades da sua aplicação
+}
+module.exports = { handleDatabaseError };
